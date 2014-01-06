34e1efd285af9ce9130b686c80cd0fe0f63ae863
Vladimir Korolyov <balodja@gmail.com>
2014-01-06 21:52:56 +0400

    Integral in the Expr with pretty printing. And a small refactor.

--- Main.hs
+++ Main.hs
@@ -4,16 +4,20 @@ import qualified Data.Vector as V
 
 import Data.List (intersperse)
 
-data Expr a = Sum (Expr a) (Expr a)
+data Expr a = Add (Expr a) (Expr a)
             | Mul (Expr a) (Expr a)
             | Atom a [Vector Int] [Vector Int] (Vector a)
+            | Integral (Expr a) Int (Limit a, Limit a)
               deriving (Eq, Read, Show)
 
+data Limit a = Zero | Infinity | Limit a
+             deriving (Eq, Read, Show)
+
 texify :: (Show a, Num a, Ord a) => Expr a -> String
-texify (Sum e1 e2) = texify e1 ++ " + " ++ texify e2
+texify (Add e1 e2) = texify e1 ++ " + " ++ texify e2
 texify (Mul e1 e2) = texifyMul e1 ++ " " ++ texifyMul e2
   where
-    texifyMul e@(Sum _ _) = "\\left( " ++ texify e ++ " \\right)"
+    texifyMul e@(Add _ _) = "\\left( " ++ texify e ++ " \\right)"
     texifyMul e = texify e
 texify (Atom k deltas units exponent)
   | null deltas && null units && V.null exponent = show k
@@ -34,13 +38,20 @@ texify (Atom k deltas units exponent)
           showNum x = let ax = abs x in if ax == 1 then [] else show ax
           cutPlus ('+' : s) = s
           cutPlus s = s
+texify (Integral e v (l1, l2)) =
+  "\\int\\limits_" ++ texifyLimit l1 ++ "^" ++ texifyLimit l2 ++ " "
+  ++ texify e ++ " \\textrm{dx}_{" ++ show (v + 1) ++ "}"
+    where
+      texifyLimit Infinity = "\\infty"
+      texifyLimit Zero = "0"
+      texifyLimit (Limit x) = show x
 
 simpleExpr :: Expr Int
 simpleExpr =
   Mul
-  (Sum
+  (Add
    (Atom 1 [V.fromList [1, 0, -1]] [V.fromList [-1, 1, 0]] (V.fromList [0, 0, 3]))
    (Atom 3 [] [V.fromList [0, 1, -1]] (V.fromList [1, 0, 2])))
   (Atom 1 [] [] (V.fromList [1, 0, 0]))
 
-main = putStrLn ("\\begin{dmath*} " ++ texify simpleExpr ++ " \\end{dmath*}")
+main = putStrLn ("$$ " ++ texify simpleExpr ++ " $$")
