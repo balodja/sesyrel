e29f0feda9f38f937325829d7ba1d8254c05ffad
Vladimir Korolyov <balodja@gmail.com>
2014-01-07 04:46:22 +0400

    Basic integration. Done with equal limits and delta-function integration.
    Next step is unit-function and exponent integration.

--- Main.hs
+++ Main.hs
@@ -1,20 +1,26 @@
+import Control.Applicative
 
 import Data.Vector (Vector)
 import qualified Data.Vector as V
 
-import Data.List (intersperse, nub)
-import Data.Maybe (fromJust)
+import Data.List (intersperse, nub, partition)
+import Data.Maybe (fromJust, maybe)
 
 data Expr a = AddE (Expr a) (Expr a)
             | MulE (Expr a) (Expr a)
-            | AtomE a [Vector Int] [Vector Int] (Vector a)
-            | IntE (Expr a) Int (Limit a, Limit a)
+            | AtomE a [Vector Int] [Vector Int] (Maybe (Vector a))
+            | IntE (Expr a) Int (Limit, Limit)
             deriving (Eq, Read, Show)
 
-data Limit a = Zero
-             | Infinity
-             | Limit (Vector a)
-             deriving (Eq, Read, Show)
+data Limit = Zero
+           | Infinity
+           | Limit (Vector Int)
+           deriving (Eq, Read, Show)
+
+
+-- some magic
+zipAlt :: Alternative f => (a -> a -> a) -> f a -> f a -> f a
+zipAlt f a b = f <$> a <*> b <|> a <|> b
 
 texify :: (Show a, Num a, Ord a) => Expr a -> String
 texify (AddE e1 e2) = texify e1 ++ " + " ++ texify e2
@@ -23,17 +29,18 @@ texify (MulE e1 e2) = texifyMul e1 ++ " " ++ texifyMul e2
     texifyMul e@(AddE _ _) = "\\left( " ++ texify e ++ " \\right)"
     texifyMul e = texify e
 texify (AtomE k deltas units exponent)
-  | null deltas && null units && V.null exponent = show k
+  | null deltas && null units && maybe False V.null exponent = show k
   | otherwise =
     (if k == 1 then [] else show k)
       ++ (concat . intersperse " " . map texifyDelta $ deltas)
       ++ (concat . intersperse " " . map texifyUnit $ units)
-      ++ texifyExponent (V.map negate exponent)
+      ++ texifyExponent (fmap (V.map negate) exponent)
         where
           texifyDelta d = "\\delta(" ++ texifyVarForm d ++ ")"
           texifyUnit u = "u(" ++ texifyVarForm u ++ ")"
-          texifyExponent e = let vf = texifyVarForm e
-                             in if null vf then [] else "e^{" ++ vf ++ "}"
+          texifyExponent Nothing = []
+          texifyExponent (Just e) = let vf = texifyVarForm e
+                                    in if null vf then [] else "e^{" ++ vf ++ "}"
 texify (IntE e v (l1, l2)) =
   "\\int\\limits_" ++ texifyLimit l1 ++ "^" ++ texifyLimit l2 ++ " "
   ++ texify e ++ " \\textrm{dx}_{" ++ show (v + 1) ++ "}"
@@ -103,9 +110,10 @@ groupify = groupifyMulAtoms
 groupifyMulAtoms :: Num a => Expr a -> Expr a
 groupifyMulAtoms e@(AtomE _ _ _ _) = e
 groupifyMulAtoms (MulE (AtomE k1 d1 u1 e1) (AtomE k2 d2 u2 e2)) =
-  AtomE (k1 * k2) (d1 ++ d2) (u1 ++ u2) (V.zipWith (+) e1 e2)
+  AtomE (k1 * k2) (d1 ++ d2) (u1 ++ u2) (zipAlt (V.zipWith (+)) e1 e2)
 groupifyMulAtoms (MulE (AtomE k1 d1 u1 e1) (MulE (AtomE k2 d2 u2 e2) e)) =
-  groupifyMulAtoms $ MulE (AtomE (k1 * k2) (d1 ++ d2) (u1 ++ u2) (V.zipWith (+) e1 e2)) e
+  groupifyMulAtoms $
+  MulE (AtomE (k1 * k2) (d1 ++ d2) (u1 ++ u2) (zipAlt (V.zipWith (+)) e1 e2)) e
 groupifyMulAtoms (MulE e1@(AtomE _ _ _ _) e2) = MulE e1 (groupifyMulAtoms e2)
 groupifyMulAtoms (MulE e1 e2) = MulE (groupifyMulAtoms e1) (groupifyMulAtoms e2)
 groupifyMulAtoms (AddE e1 e2) = AddE (groupifyMulAtoms e1) (groupifyMulAtoms e2)
@@ -116,32 +124,65 @@ substitute v vec (MulE e1 e2) = MulE (substitute v vec e1) (substitute v vec e2)
 substitute v vec (AddE e1 e2) = AddE (substitute v vec e1) (substitute v vec e2)
 substitute v vec (IntE _ _ _) = error "substitute: integral? that should not happen"
 substitute v vec (AtomE k ds us e) =
-  AtomE k (map (subForm vec) ds) (map (subForm vec) us)
-  (subForm (V.map fromIntegral vec) e)
-    where
-      subForm vec d | value == 0 = d
-                    | otherwise = V.zipWith (+) (V.map (* value) vec) (d V.// [(v, 0)])
-                      where value = d V.! v
+  AtomE k (map (substituteForm v vec) ds) (map (substituteForm v vec) us)
+  (fmap (substituteForm v . V.map fromIntegral $ vec) e)
 
-integrate :: Num a => Expr a -> (Bool, Expr a)
-integrate (MulE e1 e2) = let (b1, e1') = integrate e1
-                             (b2, e2') = integrate e2
-                         in (b1 || b2, MulE e1' e2')
-integrate (AddE e1 e2) = let (b1, e1') = integrate e1
-                             (b2, e2') = integrate e2
-                         in (b1 || b2, AddE e1' e2')
-integrate e@(AtomE _ _ _ _) = (False, e)
-integrate (IntE e v ls@(l1, l2)) = undefined
+substituteForm :: (Num a, Eq a) => Int -> Vector a -> Vector a -> Vector a
+substituteForm v vec d | value == 0 = d
+                       | otherwise = V.zipWith (+) (V.map (* value) vec) (d V.// [(v, 0)])
+                         where value = d V.! v
+
+integrateStep :: (Num a, Eq a) => Expr a -> (Bool, Expr a)
+integrateStep (MulE e1 e2) = let (b1, e1') = integrateStep e1
+                                 (b2, e2') = integrateStep e2
+                             in (b1 || b2, MulE e1' e2')
+integrateStep (AddE e1 e2) = let (b1, e1') = integrateStep e1
+                                 (b2, e2') = integrateStep e2
+                             in (b1 || b2, AddE e1' e2')
+integrateStep e@(AtomE _ _ _ _) = (False, e)
+integrateStep (IntE e v ls) = (True, integrate' e v ls)
+
+integrate' :: (Num a, Eq a) => Expr a -> Int -> (Limit, Limit) -> Expr a
+integrate' expr var ls = fromJust $ intEqualLimits <|> intDelta
+  where
+    intEqualLimits | fst ls == snd ls = Just $ AtomE 0 [] [] Nothing
+                   | otherwise = Nothing
+    intDelta = (\(vec, expr) -> MulE (calcLimitUnits var vec ls) (substitute var vec expr))
+               <$> intFindDelta expr
+    intFindDelta (AtomE k ds us e) = case partition (\d -> (d V.! var) /= 0) ds of
+      ([], rest) -> Nothing
+      (d : ds1, ds2) -> Just (calcSubstitution d, AtomE k (ds1 ++ ds2) us e)
+    intFindDelta (AddE _ _) = Nothing
+    intFindDelta (MulE e1 e2) =
+      case intFindDelta e1 of
+        Just (vec, e) -> Just (vec, MulE e e2)
+        Nothing -> case intFindDelta e2 of
+          Just (vec, e) -> Just (vec, MulE e1 e)
+          Nothing -> Nothing
+    intFindDelta (IntE _ _ _) = error "intFindDelta: integral? that should not happen"
+    
+    calcSubstitution d | d V.! var > 0 = V.map negate (d V.// [(var, 0)])
+                       | otherwise = d V.// [(var, 0)]
+    
+    calcLimitUnits :: (Num a, Eq a) => Int -> V.Vector Int -> (Limit, Limit) -> Expr a
+    calcLimitUnits var vec (l1, l2) = AtomE 1 [] (lower l1 : higher l2) Nothing
+      where
+        lower Zero = vec
+        lower Infinity = error "calcLimitUnits: lower infinite limit? wut?"
+        lower (Limit d) = V.zipWith (-) vec d
+        higher Zero = error "calcLimitUnits: higher zero limit? wut?"
+        higher Infinity = []
+        higher (Limit d) = [V.zipWith (-) d vec]
 
 distributionLambda :: Num a => Int -> Int -> a -> Expr a
 distributionLambda length variable lambda =
-  AtomE lambda [] [] (V.generate length (\i -> if i == variable then lambda else 0))
+  AtomE lambda [] [] (Just $ V.generate length (\i -> if i == variable then lambda else 0))
 
 distributionAnd :: Num a => Int -> Int -> Int -> Int -> Expr a
 distributionAnd length x a b =
   AddE
-  (AtomE 1 [V.generate length (term x b)] [V.generate length (term b a)] zero)
-  (AtomE 1 [V.generate length (term x a)] [V.generate length (term a b)] zero)
+  (AtomE 1 [V.generate length (term x b)] [V.generate length (term b a)] Nothing)
+  (AtomE 1 [V.generate length (term x a)] [V.generate length (term a b)] Nothing)
     where
       zero = V.replicate length 0
       term p m i | i == p = 1
