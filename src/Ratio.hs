e459af8998d11f58beda25bd6d96043fa44b3638
Vladimir Korolyov <balodja@gmail.com>
2014-01-25 01:09:39 +0400

    More correct Ratio.

--- src/Ratio.hs
+++ src/Ratio.hs
@@ -1,8 +1,13 @@
-module Ratio where
+module Ratio (Ratio, Rational, numerator, denominator, (%)) where
 
+import Prelude hiding (Rational)
 import qualified Data.Ratio as R (numerator, denominator, (%))
 
 data Ratio a = !a :% !a
+
+numerator, denominator :: Ratio a -> a
+numerator (x :% _) = x
+denominator (_ :% y) = y
 
 type Rational = Ratio Integer
 
@@ -13,28 +18,32 @@ instance (Num a, Eq a, Show a) => Show (Ratio a) where
   show (x :% y) = show x ++ " % " ++ show y
 
 instance Integral a => Num (Ratio a) where
-  (p1 :% 0) + (p2 :% 0) = reduce $ (p1 + p2) :% 0
+  (p1 :% 0) + (p2 :% 0) = (p1 + p2) % 0
   (p1 :% 0) + _ = p1 :% 0
   _ + (p2 :% 0) = p2 :% 0
-  (p1 :% q1) + (p2 :% q2) = reduce $ (p1 * q2 + q1 * p2) :% (q1 * q2)
-  (p1 :% 0) - (p2 :% 0) = reduce $ (p1 - p2) :% 0
+  (p1 :% q1) + (p2 :% q2) = (p1 * q2 + q1 * p2) % (q1 * q2)
+  (p1 :% 0) - (p2 :% 0) = (p1 - p2) % 0
   (p1 :% 0) - _ = p1 :% 0
   _ - (p2 :% 0) = p2 :% 0
-  (p1 :% q1) - (p2 :% q2) = reduce $ (p1 * q2 - q1 * p2) :% (q1 * q2)
-  (p1 :% q1) * (p2 :% q2) = reduce $ (p1 * p2) :% (q1 * q2)
+  (p1 :% q1) - (p2 :% q2) = (p1 * q2 - q1 * p2) % (q1 * q2)
+  (p1 :% q1) * (p2 :% q2) = (p1 * p2) % (q1 * q2)
   negate (p :% q) = (-p) :% q
   abs (p :% q) = (abs p) :% q
   signum (p :% q) = signum p :% 1
   fromInteger i = fromInteger i :% 1
 
 instance Integral a => Fractional (Ratio a) where
-  (p1 :% q1) / (p2 :% q2) = reduce $ (p1 * q2) :% (q1 * p2)
-  recip (0 :% _) = 1 :% 0
+  (p1 :% q1) / (p2 :% q2) = (p1 * q2) % (q1 * p2)
+  recip (0 :% x) = signum x :% 0
   recip (p :% q) = (q * signum p) :% (abs p)
-  fromRational r = reduce $ fromInteger (R.numerator r) :% fromInteger (R.denominator r)
+  fromRational r = fromInteger (R.numerator r) % fromInteger (R.denominator r)
 
-instance Eq a => Eq (Ratio a) where
+instance (Num a, Eq a) => Eq (Ratio a) where
+  (0 :% 0) == _ = False
+  _ == (0 :% 0) = False
   (p1 :% q1) == (p2 :% q2) = (p1 == p2) && (q1 == q2)
+  (0 :% 0) /= _ = True
+  _ /= (0 :% 0) = True
   (p1 :% q1) /= (p2 :% q2) = (p1 /= p2) || (q1 /= q2)
 
 instance (Num a, Ord a) => Ord (Ratio a) where
