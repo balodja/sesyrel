4b5e19d53e22120bbc6966bfe31c0d1df320ed82
Vladimir Korolyov <balodja@gmail.com>
2014-03-16 20:30:32 +0400

    Very large tree. Unmanageable.

--- src/Sesyrel.hs
+++ src/Sesyrel.hs
@@ -72,21 +72,41 @@ escalatorFaultTree2 = do
 
 hydrosystemsM :: FaultTreeM [Int]
 hydrosystemsM = do
-  [tank1, tank2] <- replicateM 2 (lambdaM 30)
-  [engine1, engine2] <- replicateM 2 (lambdaM 282)
+  [engineL, engineR] <- replicateM 2 (lambdaM 282)
+  [generatorL, generatorR] <- replicateM 2 (lambdaM 125)
   [electroGrp1, electroGrp2] <- replicateM 2 (lambdaM 1060)
-  [hydroPump1, hydroPump2] <- replicateM 2 (lambdaM 125)
-  electromotors <- replicateM 2 (lambdaM 100)
-  pumpStations <- replicateM 2 (lambdaM 567)
-  mechanics1 <- orM tank1 engine1
-  mechanics2 <- orM tank2 engine2
-  electroSys1 <- orM mechanics1 electroGrp1
-  electroSys2 <- orM mechanics2 electroGrp2
+  [tank1, tank2, tank3] <- replicateM 3 (lambdaM 30)
+  [hydroPump1, hydroPump2, hydroPump3] <- replicateM 3 (lambdaM 125)
+  [electromotor1, electromotor2, electromotor3, electromotor4] <- replicateM 4 (lambdaM 100)
+  [pumpStation1, pumpStation2, pumpStation3, pumpStation4] <- replicateM 4 (lambdaM 567)
+  accumulator <- lambdaM 30
+  
+  electroSys1 <- orM engineL generatorL >>= orM electroGrp1
+  electroSys2 <- orM engineL generatorR >>= orM electroGrp2
   electroSys <- andM electroSys1 electroSys2
-  hydro1Main <- orM hydroPump1 mechanics1
-  hydro2Main <- orM hydroPump1 mechanics1
-  hydro1Res <- orM electroSys (electromotors !! 0) >>= orM (pumpStations !! 0)
-  hydro2Res <- orM electroSys (electromotors !! 1) >>= orM (pumpStations !! 1)
-  hydro1 <- andM hydro1Main hydro1Res
-  hydro2 <- andM hydro2Main hydro2Res
-  return [hydro1, hydro2]
+  
+  hydro2Main <- orM electroSys electromotor2 >>= orM pumpStation2
+  hydro2Res1 <- orM accumulator electromotor4 >>= orM pumpStation4
+  let hydro2Res2 = hydroPump2
+  hydro2 <- andM hydro2Main hydro2Res1 >>= andM hydro2Res2 >>= orM tank2
+  
+  hydro1Main <- orM hydroPump1 engineL
+  hydro3Main <- orM hydroPump3 engineR
+  hydro1Res <- orM electroSys electromotor1 >>= orM pumpStation1
+  hydro3Res <- orM electroSys electromotor3 >>= orM pumpStation3
+  hydro1 <- andM hydro1Main hydro1Res >>= orM tank1
+  hydro3 <- andM hydro3Main hydro3Res >>= orM tank3
+  return [hydro1, hydro2, hydro3]
+
+actuationsM :: [Int] -> FaultTreeM [Int]
+actuationsM [hydro1, hydro2, hydro3] = do
+  [ccu1, ccu2, ccu3, ccu4] <- replicateM 4 (lambdaM 50)
+  [steer1, steer2, steer3, steer4] <- replicateM 4 (lambdaM 15)
+  channel1 <- orM hydro1 ccu1 >>= orM steer1
+  channel2 <- orM hydro2 ccu2 >>= orM steer2
+  channel3 <- orM hydro2 ccu3 >>= orM steer3
+  channel4 <- orM hydro3 ccu4 >>= orM steer4
+  elevator1 <- andM channel1 channel2
+  elevator2 <- andM channel3 channel4
+  elevator <- andM elevator1 elevator2
+  return [elevator]
