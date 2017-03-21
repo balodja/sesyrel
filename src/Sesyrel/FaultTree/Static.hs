6a31570f00dde1cc6528d75c5b8dd9b493a71021
Vladimir Korolyov <balodja@gmail.com>
2017-03-21 21:40:53 +0300

    Factor generation.

--- src/Sesyrel/FaultTree/Static.hs
+++ src/Sesyrel/FaultTree/Static.hs
@@ -2,13 +2,30 @@ module Sesyrel.FaultTree.Static
        ( ) where
 
 import qualified Data.Vector as V
+import Data.List (sort, sortBy)
+import Data.Function (on)
 
 -- The following code is a real shame.
 
-data Factor k = Factor (V.Vector Int) (Array k)
+data Factor k = Factor (V.Vector Int) (V.Vector k)
+              deriving (Show, Ord, Eq)
 
-createFactor :: [Int] -> [k] -> Factor k
-createFactor = undefined
+generateFactor :: [Int] -> ([Bool] -> k) -> Factor k
+generateFactor vars gen = Factor (V.fromList $ sort vars) (V.generate (2 ^ n) gen')
+  where
+    n = length vars
+    gen' = gen . applyPermutation invSortOrder . ind2sub n
+    sortOrder = sortingByPermutation compare vars
+    invSortOrder = inversePermutation sortOrder
+
+sortingByPermutation :: (a -> a -> Ordering) -> [a] -> [Int]
+sortingByPermutation p = map fst . sortBy (p `on` snd) . zip [0..]
+
+inversePermutation :: [Int] -> [Int]
+inversePermutation = sortingByPermutation compare
+
+applyPermutation :: [Int] -> [a] -> [a]
+applyPermutation pmt xs = map (xs !!) pmt
 
 times :: Factor k -> Factor k -> Factor k
 times = undefined
@@ -16,32 +33,11 @@ times = undefined
 eliminate :: Int -> Factor k
 eliminate = undefined
 
-data Array k = Array {
-    arraySize :: V.Vector Int
-  , arrayData :: V.Vector k
-  } deriving (Show, Eq, Ord)
-
-size :: Array k -> V.Vector Int
-size = arraySize
-
-toVector :: Array k -> V.Vector k
-toVector = arrayData
-
-fromVector :: V.Vector k -> Array k
-fromVector v = Array (V.singleton $ V.length v) v
-
-numel :: Array k -> Int
-numel (Array sz _) = V.product sz
-
-ind2sub :: V.Vector Int -> Int -> V.Vector Int
-ind2sub sz i = V.map snd $ V.postscanl' go (i, 0) sz
+ind2sub :: Int -> Int -> [Bool]
+ind2sub n = take n . go
   where
-    go (ind, _) sz = ind `divMod` sz
-
-sub2ind :: V.Vector Int -> V.Vector Int -> Int
-sub2ind sz v = V.foldr' (\(s, i) x -> i + s * x) 0 $ V.zip sz v
-
-
+    go i = let (p, q) = i `divMod` 2
+           in (q /= 0) : go p
 
 compileFaultTreeStatic :: ()
 compileFaultTreeStatic = ()
