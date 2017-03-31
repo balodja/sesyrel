ed3791fb8aa27a1ff78b894dde9ce288de8ce84d
Vladimir Korolyov <balodja@gmail.com>
2017-03-31 18:04:52 +0300

    Gate logic for doing fair and true comparison in voting gates. Complicated one.

--- src/Sesyrel/FaultTree/Base.hs
+++ src/Sesyrel/FaultTree/Base.hs
@@ -18,8 +18,12 @@ module Sesyrel.FaultTree.Base (
   , variablesM
   , addNodeM
   , lambdaM, constantM
-  , notM, andM, orM
-  , voterM
+  , notM, andM, orM, xorM
+  , oneAdderM, fullOneAdderM
+  , adderM, compiledAdderM
+  , sumM, equalsM, notLessM
+  , voterM, voterEqualsM
+  , naiveVoterM
   , priorityAndOrM
   , switchM
   ) where
@@ -32,6 +36,7 @@ import Control.Monad.State
 import Control.Monad.Identity
 import Data.Foldable (foldl')
 import Data.Text (Text)
+import Data.Maybe (catMaybes)
 
 type FaultTreeMonadT k m = StateT (FaultTree k) m
 
@@ -57,6 +62,7 @@ data FaultTreeNode k = FaultTreeLambda k
                      | FaultTreeNot Variable
                      | FaultTreeAnd Variable Variable
                      | FaultTreeOr Variable Variable
+                     | FaultTreeXor Variable Variable
                      | FaultTreePriorityAndOr Variable Variable Variable
                      | FaultTreeSwitch Variable Variable Variable
                      deriving (Show, Eq)
@@ -69,6 +75,7 @@ isDynamic (FaultTree vs) = any isDynamic' $ map snd vs
     isDynamic' (FaultTreeNot _) = False
     isDynamic' (FaultTreeAnd _ _) = False
     isDynamic' (FaultTreeOr _ _) = False
+    isDynamic' (FaultTreeXor _ _) = False
     isDynamic' (FaultTreePriorityAndOr _ _ _) = True
     isDynamic' (FaultTreeSwitch _ _ _) = True
 
@@ -86,18 +93,149 @@ constantM = addNodeM . FaultTreeConstant
 notM :: Monad m => Variable -> FaultTreeMonadT k m Variable
 notM = addNodeM . FaultTreeNot
 
-andM, orM :: Monad m => Variable -> Variable -> FaultTreeMonadT k m Variable
+andM, orM, xorM :: Monad m => Variable -> Variable -> FaultTreeMonadT k m Variable
 andM a b = addNodeM $ FaultTreeAnd a b
 orM a b = addNodeM $ FaultTreeOr a b
+xorM a b = addNodeM $ FaultTreeXor a b
 
-voterM :: (Num k, Monad m) => Int -> [Variable] -> FaultTreeMonadT k m Variable
-voterM k vars = do
+foldVarsM :: Monad m => (Variable -> Variable -> FaultTreeMonadT k m Variable) ->
+             [Variable] -> FaultTreeMonadT k m (Maybe Variable)
+foldVarsM f [] = return Nothing
+foldVarsM f vs = foldM (\mb v -> Just <$> maybe (return v) (f v) mb) Nothing vs
+
+naiveVoterM :: (Num k, Monad m) => Int -> [Variable] -> FaultTreeMonadT k m Variable
+naiveVoterM k vars = do
   initial <- replicateM k (constantM 0)
   let step voters v = do
         v0 <- constantM 1
         zipWithM (\u u' -> andM v u' >>= orM u) voters (v0 : voters)
   final <- foldM step initial vars
   return $ last final
+
+oneAdderM :: Monad m => Variable -> Variable -> FaultTreeMonadT k m (Variable, Variable)
+oneAdderM a b = do
+  s <- xorM a b
+  c <- andM a b
+  return (s, c)
+
+fullOneAdderM :: Monad m =>
+             Variable -> Variable ->
+             Variable -> FaultTreeMonadT k m (Variable, Variable)
+fullOneAdderM a b c = do
+  aXb <- xorM a b
+  s <- xorM aXb c
+  aXbc <- andM aXb c
+  ab <- andM a b
+  c' <- orM ab aXbc -- xorM ab aXbc also possible, as c = ab `xor` bc `xor` ac
+  return (s, c')
+
+onePlusOneAdderM :: Monad m => Variable -> Variable -> FaultTreeMonadT k m (Variable, Variable)
+onePlusOneAdderM a b = do
+  notB <- notM b
+  s <- xorM a notB
+  aNotB <- andM a notB
+  c <- xorM aNotB b
+  return (s, c)
+
+adderM :: Monad m => [Variable] -> [Variable] -> FaultTreeMonadT k m ([Variable], Maybe Variable)
+adderM xs' ys' = go xs' ys' Nothing
+  where
+    go (x : xs) (y : ys) c = do
+      (z, c') <- maybe (oneAdderM x y) (fullOneAdderM x y) c
+      (as, c'') <- go xs ys (Just c')
+      return (z : as, c'')
+    go [] (y : ys) (Just c) = do
+      (z, c') <- oneAdderM y c
+      (as, c'') <- go [] ys (Just c')
+      return (z : as, c'')
+    go [] ys Nothing = return (ys, Nothing)
+    go xs [] c = go [] xs c
+
+compiledAdderM :: Monad m => Int -> [Variable] -> FaultTreeMonadT k m ([Variable], Maybe Variable)
+compiledAdderM k' vars = go k' vars Nothing
+  where
+    go k (x : xs) (Just c) = do
+      (s, c') <- if k `mod` 2 == 0
+                 then oneAdderM x c
+                 else onePlusOneAdderM x c
+      (ss, c'') <- go (k `div` 2) xs (Just c')
+      return (s : ss, c'')
+    go k (x : xs) Nothing = do
+      (s, c') <- if k `mod` 2 == 0
+                 then return (x, Nothing)
+                 else do {s <- notM x; return (s, Just x)}
+      (ss, c'') <- go (k `div` 2) xs c'
+      return (s : ss, c'')
+    go _ [] c = return ([], c)
+
+extractVariables :: Maybe Variable -> Maybe Variable -> (Variable, Variable)
+extractVariables (Just a) (Just c) = (a, c)
+extractVariables Nothing _ = error "equalsM, notLessM: 0-order logic, don't"
+extractVariables _ Nothing = error "equalsM, notLessM: comparison to (2^order) is not possible"
+
+addForComparisonM :: Monad m => Int -> [Variable] -> FaultTreeMonadT k m (Variable, Variable)
+addForComparisonM k vars = do
+  let n = length vars
+  (vars', cMb) <- compiledAdderM (2 ^ n - k) vars
+  anyMb <- foldVarsM orM vars'
+  return $ extractVariables anyMb cMb
+
+equalsM :: Monad m => Int -> [Variable] -> FaultTreeMonadT k m Variable
+equalsM k vars = do
+  (any', c) <- addForComparisonM k vars
+  notAny' <- notM any'
+  notC <- notM c
+  andM notAny' notC
+
+notLessM :: Monad m => Int -> [Variable] -> FaultTreeMonadT k m Variable
+notLessM k vars = do
+  (_, c) <- addForComparisonM k vars
+  return c
+
+sumM :: Monad m => Int -> [Variable] -> FaultTreeMonadT k m ([Variable], Maybe Variable)
+sumM n vars = do {ps <- split vars; doSum ps}
+  where
+    split (v1 : v2 : v3 : vs) = do {(s, c) <- fullOneAdderM v1 v2 v3; r <- split vs; return $ ([s, c], Nothing) : r}
+    split [v1, v2] = do {(s, c) <- oneAdderM v1 v2; return [([s, c], Nothing)]}
+    split [v1] = return [([v1], Nothing)]
+    split [] = return [([], Nothing)]
+    doSum [p] = return p
+    doSum [] = return ([], Nothing)
+    doSum ps = do {ps' <- step ps; doSum ps'}
+    step (p1 : p2 : ps) = do {p' <- sumTwo p1 p2; ps' <- step ps; return (p' : ps')}
+    step [p] = return [p]
+    step [] = return []
+    sumTwo (vs1, Nothing) (vs2, Nothing) = do
+      (vs3, c3) <- adderM vs1 vs2
+      case c3 of
+        Just c -> return $ if length vs3 == n
+                           then (vs3, Just c)
+                           else (vs3 ++ [c], Nothing)
+        Nothing -> return (vs3, Nothing)
+    sumTwo (vs1, c1) (vs2, c2) = do
+      (vs3, c3) <- adderM vs1 vs2
+      let cs = catMaybes [c1, c2, c3]
+      c <- foldVarsM orM cs
+      return (vs3, c)
+
+bitsN :: Int -> Int
+bitsN k = ceiling . (\x -> log (x :: Double) / log 2) $ 1 + realToFrac k
+
+voterEqualsM :: Monad m => Int -> [Variable] -> FaultTreeMonadT k m Variable
+voterEqualsM k vars = do
+  (sumVars, cfMb) <- sumM (bitsN k) vars
+  fVar <- equalsM k sumVars
+  case cfMb of
+    Nothing -> return fVar
+    Just cf -> do { notCf <- notM cf; andM notCf fVar }
+
+voterM :: Monad m => Int -> [Variable] -> FaultTreeMonadT k m Variable
+voterM k vars = do
+  (sumVars, cfMb) <- sumM (bitsN k) vars
+  fVar <- notLessM k sumVars
+  case cfMb of
+    Nothing -> return fVar
+    Just cf -> orM fVar cf
 
 priorityAndOrM, switchM :: Monad m => Variable -> Variable -> Variable -> FaultTreeMonadT k m Variable
 priorityAndOrM a b c = addNodeM $ FaultTreePriorityAndOr a b c
@@ -121,6 +259,7 @@ faultTreeVariables (FaultTree ps) = map (\(v, node) -> v : nodeVariables node) p
     nodeVariables (FaultTreeNot x) = [x]
     nodeVariables (FaultTreeAnd x y) = [x, y]
     nodeVariables (FaultTreeOr x y) = [x, y]
+    nodeVariables (FaultTreeXor x y) = [x, y]
     nodeVariables (FaultTreePriorityAndOr x y z) = [x, y, z]
     nodeVariables (FaultTreeSwitch s x y) = [s, x, y]
 
