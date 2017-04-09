9d97a915cf257dd46e527e61887e86b9814e33eb
Vladimir Korolyov <balodja@gmail.com>
2017-04-09 15:42:20 +0300

    Reimplementation of the VE algorithms. One really bad bug eliminated (removeVertex was not adding the new edges).

--- src/Sesyrel/FaultTree/Elimination.hs
+++ src/Sesyrel/FaultTree/Elimination.hs
@@ -2,82 +2,93 @@ module Sesyrel.FaultTree.Elimination (findOrdering, pretend, Algorithm(..)) wher
 
 import Sesyrel.FaultTree.Base (Variable)
 
-import Data.List (partition, nub, sort, union, delete, elemIndex)
-import Data.Maybe (fromJust, fromMaybe)
+import Data.Function (on)
+import Data.Foldable
+import Data.Maybe (fromMaybe)
+import Data.List (partition)
 
-import Data.Map (Map)
-import qualified Data.Map as M
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
 
-type Graph = Map Variable [Variable]
+import Data.Set (Set)
+import qualified Data.Set as S
+
+type Clique = Set Variable
+type Graph = Map Variable (Set Variable)
 
 data Algorithm = GraphMinFill | GraphMinNeighbors | MinCardinality
 
 pretend :: [Variable] -> [[Variable]] -> [[[Variable]]]
-pretend [] cliques = [filter (not . null) cliques]
-pretend (v : vs) cliques =
-  let cs = filter (not . null) cliques
-      (c, rest) = escapeClique v cs
-  in cs : pretend vs (c : rest)
+pretend vs = map (map S.toList) . pretend' vs . map S.fromList
 
-escapeClique :: Variable -> [[Variable]] -> ([Variable], [[Variable]])
+pretend' :: [Variable] -> [Clique] -> [[Clique]]
+pretend' [] cliques = [cliques]
+pretend' (v : vs) cliques =
+  let (c, rest) = escapeClique v cliques
+  in cliques : pretend' vs (if null c then rest else c : rest)
+
+escapeClique :: Variable -> [Clique] -> (Clique, [Clique])
 escapeClique v cliques =
   let (yes, no) = partition (elem v) cliques
-      c = delete v . nub . sort . foldl union [] $ yes
+      c = S.delete v $ fold yes
   in (c, no)
 
-findOrdering :: Maybe Algorithm -> [Variable] -> [[Variable]] -> [Variable]
-findOrdering Nothing = findOrdering (Just MinCardinality)
-findOrdering (Just GraphMinFill) = findGraphOrdering costFunctionMinFill
-findOrdering (Just GraphMinNeighbors) = findGraphOrdering costFunctionMinNeighbors
-findOrdering (Just MinCardinality) = findMinCardinalityOrdering
+findOrdering :: Maybe Algorithm -> [Variable] -> [[Variable]] -> [(Variable, Int)]
+findOrdering alg vs cs = findOrdering' alg (S.fromList vs) $ map S.fromList cs
 
-findMinCardinalityOrdering :: [Variable] -> [[Variable]] -> [Variable]
-findMinCardinalityOrdering [] _ = []
-findMinCardinalityOrdering vs cliques =
-  let costs = map (\v' -> length . fst $ escapeClique v' cliques) vs
-      v = (vs !!) . fromJust $ elemIndex (minimum costs) costs
+findOrdering' :: Maybe Algorithm -> Set Variable -> [Clique] -> [(Variable, Int)]
+findOrdering' Nothing = findOrdering' (Just MinCardinality)
+findOrdering' (Just GraphMinFill) = findGraphOrdering costFunctionMinFill
+findOrdering' (Just GraphMinNeighbors) = findGraphOrdering costFunctionMinNeighbors
+findOrdering' (Just MinCardinality) = findMinCardinalityOrdering
+
+findMinCardinalityOrdering :: Set Variable -> [Clique] -> [(Variable, Int)]
+findMinCardinalityOrdering vs cliques | S.null vs = []
+                                      | otherwise =
+  let costFun v' = length . fst $ escapeClique v' cliques
+      v = getNextVertex costFun vs
       (c, rest) = escapeClique v cliques
-  in v : findMinCardinalityOrdering (delete v vs) (c : rest)
+      sz = length rest
+      continue = findMinCardinalityOrdering (S.delete v vs) (c : rest)
+  in v `seq` sz `seq` ((v, sz) : continue)
 
-findGraphOrdering :: (Graph -> Variable -> Int) -> [Variable] -> [[Variable]] -> [Variable]
+findGraphOrdering :: (Graph -> Variable -> Int) -> Set Variable -> [Clique] -> [(Variable, Int)]
 findGraphOrdering costFunction vars cliques = go vars (makeGraph cliques)
   where
-    go [] _ = []
-    go vs g = let v = getNextVertex costFunction vs g
-              in v : go (delete v vs) (removeVertex v g)
+    go vs g | S.null vs = []
+            | otherwise = let v = getNextVertex (costFunction g) vs
+                              (g', sz) = removeVertex v g
+                          in v `seq` sz `seq` ((v, sz) : go (S.delete v vs) g')
 
-getNextVertex :: (Graph -> Variable -> Int) -> [Variable] -> Graph -> Variable
-getNextVertex f vs g = let costs = map (f g) vs
-                       in (vs !!) . fromJust $ elemIndex (minimum costs) costs
+getNextVertex :: (Variable -> Int) -> Set Variable -> Variable
+getNextVertex f vs = let costs = S.mapMonotonic (\v -> (v, f v)) vs
+                     in fst $ minimumBy (compare `on` snd) costs
 
-addClique :: [Variable] -> Graph -> Graph
-addClique [] = id
-addClique (v : vs) = foldr ((.) . addEdge v) id vs . addClique vs
+addClique :: Graph -> Clique -> Graph
+addClique graph clique = foldl' (flip addEdge) graph edges
+  where
+    f = S.toList clique
+    edges = [(v1, v2) | v1 <- f, v2 <- f, v1 /= v2]
 
-addEdge :: Variable -> Variable -> Graph -> Graph
-addEdge a b | a == b = id 
-            | otherwise = M.insertWith (\_ ns -> if b `elem` ns then ns else b : ns) a [b]
-              . M.insertWith (\_ ns -> if a `elem` ns then ns else a : ns) b [a]
+addEdge :: (Variable, Variable) -> Graph -> Graph
+addEdge (a, b) = M.insertWith S.union a (S.singleton b)
 
-removeEdge :: Variable -> Variable -> Graph -> Graph
-removeEdge a b = M.adjust (delete b) a
-                 . M.adjust (delete a) b
+removeEdge :: (Variable, Variable) -> Graph -> Graph
+removeEdge (a, b) = M.adjust (S.delete b) a
 
-removeVertex :: Variable -> Graph -> Graph
-removeVertex v g = let ns = fromMaybe [] (M.lookup v g)
-                   in M.delete v . foldr ((.) . removeEdge v) id ns $ g
+removeVertex :: Variable -> Graph -> (Graph, Int)
+removeVertex a g = let ns = fromMaybe S.empty (M.lookup a g)
+                       sz = length ns
+                   in ((`addClique` ns) . M.delete a $ foldl' (\g' b -> removeEdge (b, a) $ removeEdge (a, b) g') g ns, sz)
 
-makeGraph :: [[Variable]] -> Graph
-makeGraph cliques = foldr ((.) . addClique) id cliques M.empty
+makeGraph :: [Clique] -> Graph
+makeGraph cliques = foldl' addClique M.empty cliques
 
 costFunctionMinFill :: Graph -> Variable -> Int
 costFunctionMinFill g v =
-  let neighs = fromMaybe [] (M.lookup v g)
-      n = length neighs
-      edge k = map ((,) k) $ g M.! k
-      inClique (a, b) = elem a neighs && elem b neighs
-      neighEdges2 = length . filter inClique . concatMap edge $ neighs
-  in n * (n - 1) - neighEdges2
+  let neighs = fromMaybe S.empty (M.lookup v g)
+      edgeNum k = S.size $ S.difference neighs (g M.! k)
+  in sum . map edgeNum . S.toList $ neighs
 
 costFunctionMinNeighbors :: Graph -> Variable -> Int
-costFunctionMinNeighbors g v = length . fromMaybe [] . M.lookup v $ g
+costFunctionMinNeighbors g v = S.size . fromMaybe S.empty . M.lookup v $ g
